988d810c904324382396e56a2e50b51036b0774e
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-12-01 19:38:21 -0500

    added name command

--- client.js
+++ client.js
@@ -1,17 +1,24 @@
 // establishes a connection with the game server
+const net = require("net")
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541,
   });
 
+  conn.write("Name: MyS");
+
   conn.on("connect", () => {
-    console.log("connect scusses");
+    console.log("Successfully connected to game server");
   });
 
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
+
+   conn.on("data", () => {
+     console.log("Name: MYS");
+   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
